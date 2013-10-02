1d6152531bbc7562b6dc5fe42cb6ffdbc4df7159
Samium Gromoff <_deepfire@feelingofgreen.ru>
2013-10-03 01:20:43 +0400

    Wrap glib and gdk-pixbuf as well, since Clutter is mostly useless without them

--- clutter-raw.lisp
+++ clutter-raw.lisp
@@ -1,22 +1,27 @@
-(cl:defpackage :clutter-raw
-  (:use :autowrap))
-
-(cl:in-package :clutter-raw)
+(defpackage clutter-raw)
+(in-package :clutter-raw)
 
 (cl:eval-when (:compile-toplevel :load-toplevel :execute)
   (cffi-sys:%load-foreign-library
+   :libclutter "/usr/lib64/libgdk_pixbuf-2.0.so")
+  (cffi-sys:%load-foreign-library
+   :libclutter "/usr/lib64/libglib-2.0.so")
+  (cffi-sys:%load-foreign-library
    :libclutter "/usr/lib64/libclutter-1.0.so.0"))
 
-(autowrap:c-include "clutter-raw.h"
+(autowrap:c-include (cl:namestring (asdf:system-relative-pathname (asdf:find-system :cffi-clutter)
+                                                               "clutter-raw.h"))
                     :exclude-arch ("i686-pc-linux-gnu"
                                    "i686-pc-win32"
                                    "x86_64-pc-win64"
                                    "i686-apple-darwin9"
                                    "x86_64-apple-darwin9")
-                    :exclude-sources ("/usr/local/lib/clang/3.3/include/(?!stddef.h)"
-                                      "/usr/include/(?!stdint.h|bits/types.h|sys/types.h|glib-2.0|clutter-1.0|cogl).*"
+                    :exclude-sources ("/usr/include/(?:getopt|pthread|sched|stdio|time|unistd|xlocale).h"
+                                      ;; "/usr/include/(?!stdint.h|/home/deepfire/src/cffi-clutter/bits/types.h|sys/types.h|glib-2.0|clutter-1.0|cogl).*"
                                       "/usr/include/glib-2.0/glib/deprecated")
+                    :exclude-definitions ("^signal$")
                     :sysincludes '("/usr/include/"
+                                   "/usr/include/linux"
                                    "/usr/include/glib-2.0"
                                    "/usr/lib64/glib-2.0/include"
                                    "/usr/include/cairo"
@@ -24,7 +29,7 @@
                                    "/usr/include/clutter-1.0"
                                    "/usr/include/pango-1.0"
                                    "/usr/include/atk-1.0"
+                                   "/usr/include/gdk-pixbuf-2.0"
                                    "/usr/include/json-glib-1.0")
                     :exclude-constants (".*")
-                    :constant-accessor clutter-constant)
-
+                    :constant-accessor const)
